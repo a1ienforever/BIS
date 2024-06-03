4347d970592993243e76a8c4efc340512335d1cc
alienforever <artyom.zadvornyj@yandex.ru>
2024-06-03 18:31:39 +0300

    all lb complete

--- lb7/TakeGrant.py
+++ lb7/TakeGrant.py
@@ -1,51 +1,106 @@
-class TakeGrantModel:
-    def __init__(self):
-        self.graph = {}
+def de_jure_closure(S, O, E):
+    # Создаем копию множества ребер для работы
+    Etg = set(E)
+    changed = True
 
-    def add_subject(self, subject):
-        if subject not in self.graph:
-            self.graph[subject] = {}
+    # Повторяем, пока добавляются новые ребра
+    while changed:
+        changed = False
+        new_edges = set()
 
-    def add_object(self, subject, obj):
-        if subject in self.graph:
-            self.graph[subject][obj] = []
+        # Применяем правило take
+        for (x, y, t) in Etg:
+            if t == 't':
+                for (y2, z, α) in Etg:
+                    if y == y2 and (x, z, α) not in Etg:
+                        new_edges.add((x, z, α))
 
-    def add_right(self, subject, obj, right):
-        if subject in self.graph and obj in self.graph[subject]:
-            self.graph[subject][obj].append(right)
+        # Применяем правило grant
+        for (x, y, g) in Etg:
+            if g == 'g':
+                for (y2, z, α) in Etg:
+                    if y == y2 and (x, z, α) not in Etg:
+                        new_edges.add((x, z, α))
 
-    def verify_leak(self, subject, target_obj, visited=None):
-        if visited is None:
-            visited = set()
+        # Обновляем множество ребер, если были добавлены новые
+        if new_edges:
+            Etg.update(new_edges)
+            changed = True
 
-        visited.add(subject)
+    return Etg
 
-        if subject not in self.graph:
-            return False
 
-        for obj, rights in self.graph[subject].items():
-            if obj == target_obj:
-                return True
-            for right in rights:
-                if right == "grant" and obj not in visited:
-                    if self.verify_leak(obj, target_obj, visited):
-                        return True
+def de_facto_closure(S, O, E, F):
+    # Определяем правила де-факто
+    defacto_rules = {('w', 'spy'), ('w', 'find'), ('r', 'post'), ('r', 'pass')}
 
-        return False
+    # Получаем де-юре замыкание
+    Es_de_jure = de_jure_closure(S, O, E)
+    F_new = set(F)
 
+    # Применяем первые два де-факто правила
+    for (x, y, α) in Es_de_jure:
+        if α in {'w', 'r'}:
+            for rule in defacto_rules:
+                if α == rule[0] and (x, y, rule[1]) not in F_new:
+                    F_new.add((x, y, rule[1]))
+
+    # Инициализируем список ребер и множество вершин
+    L = list(F_new)
+    N = set()
+
+    # Повторяем, пока список L не пуст
+    while L:
+        (x, y, α) = L.pop(0)
+        N.add(y)
+        for z in S:
+            for (x2, y2, β) in Es_de_jure:
+                if y == x2 and (x, z, β) not in F_new:
+                    F_new.add((x, z, β))
+                    L.append((x, z, β))
+
+    return F_new
+
+
+def full_closure(S, O, E, F):
+    # Получаем де-юре замыкание
+    Etg = de_jure_closure(S, O, E)
+    # Получаем де-факто замыкание
+    F_new = de_facto_closure(S, O, E, F)
+    # Объединяем де-юре и де-факто замыкания
+    return Etg.union(F_new)
 
 if __name__ == '__main__':
-    model = TakeGrantModel()
+    # Пример 1
+    S1 = {'A', 'B'}
+    O1 = {'obj1', 'obj2'}
+    E1 = {('A', 'obj1', 't'), ('A', 'obj1', 'g'), ('B', 'obj2', 't')}
+    F1 = set()
 
-    model.add_subject("UserA")
-    model.add_subject("UserB")
+    # Пример 2
+    S2 = {'C', 'D', 'E'}
+    O2 = {'obj3', 'obj4'}
+    E2 = {('C', 'obj3', 'r'), ('D', 'obj3', 'w'), ('E', 'obj4', 't'), ('C', 'D', 'g')}
+    F2 = set()
 
-    model.add_object("UserA", "File1")
-    model.add_object("UserA", "File2")
+    # Выполнение замыканий для примера 1
+    Etg_closure_1 = de_jure_closure(S1, O1, E1)
+    F_closure_1 = de_facto_closure(S1, O1, E1, F1)
+    full_closure_result_1 = full_closure(S1, O1, E1, F1)
 
-    model.add_right("UserA", "File1", "read")
-    model.add_right("UserA", "File1", "write")
-    model.add_right("UserA", "File2", "read")
+    # Выполнение замыканий для примера 2
+    Etg_closure_2 = de_jure_closure(S2, O2, E2)
+    F_closure_2 = de_facto_closure(S2, O2, E2, F2)
+    full_closure_result_2 = full_closure(S2, O2, E2, F2)
 
-    print(model.verify_leak("UserA", "File2"))
-    print(model.verify_leak("UserB", "File1"))
+    # Вывод результатов для примера 1
+    print("Пример 1:")
+    print("Де-юре замыкание:", Etg_closure_1)
+    print("Де-факто замыкание:", F_closure_1)
+    print("Полное замыкание:", full_closure_result_1)
+
+    # Вывод результатов для примера 2
+    print("\nПример 2:")
+    print("Де-юре замыкание:", Etg_closure_2)
+    print("Де-факто замыкание:", F_closure_2)
+    print("Полное замыкание:", full_closure_result_2)
